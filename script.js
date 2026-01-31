0d059cea5d2c2691c2f45c307df7d3cec951c74d
Tushar Raghuvanshi <79913268+T7351@users.noreply.github.com>
2026-01-31 09:50:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere() {
         const radius =document.getElementById("radius").value;
         const volume = (4/3) * Math.PI * Math.pow(radius, 3);
-        document.getElementById('volume').value = volume.toFixed(2);
+        document.getElementById('volume').value = volume.toFixed(3);
         return false;
 } 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
